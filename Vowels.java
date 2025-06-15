174c386ce79479f06b455d86853650b4f8f2afce
Vinuthna-123 <119519144+Vinuthna-123@users.noreply.github.com>
2025-06-15 12:38:28 +0530

    Update Vowels.java

--- Vowels.java
+++ Vowels.java
@@ -17,24 +17,3 @@ public class Vowels {
             System.out.println("Number of vowels in the string: " + vowelCount);
         }
     }
-
-    // public class ConsonantCounter {
-    //     public static int countConsonants(String str) {
-    //         str = str.toLowerCase();
-    //         int count = 0;
-    //         for (int i = 0; i < str.length(); i++) {
-    //             char ch = str.charAt(i);
-    //             if ((ch >= 'a' && ch <= 'z') && !(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')) {
-    //                 count++;
-    //             }
-    //         }
-    //         return count;
-    //     }
-    
-    //     public static void main(String[] args) {
-    //         String input = "Hello, world!";
-    //         int consonantCount = countConsonants(input);
-    //         System.out.println("Number of consonants in the string: " + consonantCount);
-    //     }
-    // }
-    
